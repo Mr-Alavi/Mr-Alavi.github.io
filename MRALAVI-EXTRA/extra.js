19e9524a92842ac096bffc58869df1636a9c81b4
Mr.Alavi <x.mralavi.x@gmail.com>
2026-07-18 19:03:44 +0330

    Delete MRALAVI-EXTRA directory

--- MRALAVI-EXTRA/extra.js
+++
@@ -1,9 +0,0 @@
-function showNotification(msg) {
-    const el = document.createElement('div');
-    el.className = 'mralavi-notification';
-    el.innerText = msg;
-    document.body.appendChild(el);
-    setTimeout(() => el.remove(), 3000);
-}
-// ثبت PWA
-if ('serviceWorker' in navigator) navigator.serviceWorker.register('service-worker.js');
